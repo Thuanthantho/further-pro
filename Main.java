8a2d7505f20e4736e4a4886e82303a6435d022fa
StuHung <93218054+StuHung@users.noreply.github.com>
2023-05-02 15:59:39 +0700

    Delete Main.java

--- Main.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(PhysicalProduct.phys); 
-        System.out.println(DigitalProduct.dig1); 
-        System.out.println(DigitalProduct.dig2); 
-
-    }
-}
